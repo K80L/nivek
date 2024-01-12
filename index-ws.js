a95648a6c828497759b92d5c04199e8e6222dd44
Kevin Ly <kly@newrelic.com>
2024-01-11 16:24:12 -0800

    asdfasdf

--- index-ws.js
+++ index-ws.js
@@ -18,6 +18,21 @@ const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({ server: server });
 
+// Listener on the signal interrupt event (CTRL+C)
+// server.close does:
+//    - stops server from accepting new connections
+//    - keeps current connections alive
+//    - when all connections are closed -> server will emit a `close` event
+//    - when `close` event is emitted -> callback is called
+process.on("SIGINT", () => {
+  wss.clients.forEach((client) => {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
   console.log("Clients connected", numClients);
@@ -27,6 +42,10 @@ wss.on("connection", function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send("Welcome to my server");
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
 
   ws.on("close", function close() {
     console.log("A client has disconnected");
@@ -38,3 +57,33 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** END WEBSOCKETS */
+
+/** START DATABASE */
+
+const sqlite = require("sqlite3");
+
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  // need to use db.run() whenever you want to run a SQL command
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down db");
+  db.close();
+}
